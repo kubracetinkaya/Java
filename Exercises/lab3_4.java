094e55a57ad75a32ea70ee63272dd7b21d197af9
Kübra Çetinkaya <35381438+kubracetinkaya@users.noreply.github.com>
2018-02-28 20:50:21 +0200

    Update and rename lab3_4.java to Exercises/lab3_4.java

--- Exercises/lab3_4.java
+++ Exercises/lab3_4.java
@@ -1,21 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package lab3;
 
 import java.util.Scanner;
 
-/**
- *
- * @author asus
- */
+
 public class lab3_4 {
 
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("((a*x*x)+(b*x)+c) polinomunda a katsayısını giriniz");
