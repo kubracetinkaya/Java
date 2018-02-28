3ec428aeb375496e2f43e9bd14b5040471b48545
Kübra Çetinkaya <35381438+kubracetinkaya@users.noreply.github.com>
2018-02-28 20:51:58 +0200

    Update and rename lab4_2.java to Exercises/lab4_2.java

--- Exercises/lab4_2.java
+++ Exercises/lab4_2.java
@@ -1,21 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package lab4;
 
 import java.util.Scanner;
 
-/**
- *
- * @author asus
- */
+
 public class lab4_2 {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int y = 0;
