899565c4b1e08643d6b3d7b7c1587f49e0c900b8
Kübra Çetinkaya <35381438+kubracetinkaya@users.noreply.github.com>
2018-03-01 21:16:31 +0200

    Add files via upload

---
+++ UygulamaWhileFor.java
@@ -0,0 +1,111 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uygulama.pkgwhile.pkgfor;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author asus
+ */
+public class UygulamaWhileFor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+//        int x = -20;
+//        while (x < 150) {
+//            System.out.println(x);
+//            x += 3;
+//        int y = 270;
+//        while (y > 13) {
+//            System.out.println(y);
+//            y -= 7;
+//        }
+//        float z=0;
+//        while(z<1){
+//            System.out.println(z);
+//            z+=0.05;
+//    }
+//        
+        //soru10
+//        System.out.println("pozitif bir sayı giriniz");
+//        double sayı = in.nextDouble();
+//
+//        while (sayı < 0) {
+//
+//            System.out.println("Negatif bir sayı girdiniz.");
+//            break;
+//        }
+//        System.out.println("bir sayı daha giriniz");
+//        double sayı2 = in.nextDouble();
+//
+//        while (sayı2 == 0) {
+//            System.out.println("Sayınız sıfır.");
+//            break;
+//        }
+//        System.out.println("pozitif bir sayı daha girirniz");
+//        double sayı3 = in.nextDouble();
+//
+//        while (sayı3 > 0) {
+//            System.out.println("Tebrikler...Pozitif bir sayı girdiniz.");
+//            break;
+//        }
+
+        //soru11
+//        System.out.println("Negatif bir sayı giriniz");
+//        double sayı = in.nextDouble();
+//        double toplam = 0;
+//        toplam = toplam + sayı;
+//
+//        while (sayı > 0) {
+//
+//            System.out.println("Pozitif bir sayı girdiniz.");
+//            break;
+//        }
+//        System.out.println("bir sayı daha giriniz");
+//        double sayı2 = in.nextDouble();
+//        toplam = toplam + sayı2;
+//        while (sayı2 == 0) {
+//            System.out.println("Sayınız sıfır.");
+//            break;
+//        }
+//        System.out.println("Negatif bir sayı daha girirniz");
+//        double sayı3 = in.nextDouble();
+//        toplam = toplam + sayı3;
+//        while (sayı3 < 0) {
+//            System.out.println("Tebrikler...Negatif bir sayı girdiniz.");
+//            break;
+//
+//        }
+//        System.out.println("sayıların toplamı" + toplam);
+        //soru12
+//        double x = 1;
+//
+//        while (x > 0.001) {
+//            System.out.println(x);
+//            x = x /= 2;
+//        }
+
+        
+        
+        
+        
+        //soru13
+        
+        
+        
+        
+        //soru14
+       
+            
+            
+        }
+    }
+
+
