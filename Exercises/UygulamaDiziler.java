0a7220d938c78706785a399506e5b2af66c27228
Kübra Çetinkaya <35381438+kubracetinkaya@users.noreply.github.com>
2018-03-01 21:23:40 +0200

    Update and rename UygulamaDiziler.java to Exercises/UygulamaDiziler.java

--- Exercises/UygulamaDiziler.java
+++ Exercises/UygulamaDiziler.java
@@ -1,21 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package uygulama.diziler;
 
 import java.util.Scanner;
 
-/**
- *
- * @author asus
- */
+
 public class UygulamaDiziler {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
        // Scanner in = new Scanner(System.in);
         
@@ -29,7 +20,7 @@ public class UygulamaDiziler {
 //        System.out.println(dizi3[0] + " " + dizi3[N-1]);
         
         
-        //20
+        
 //        int i=0;
 //        for (double j =1; j <=N; j++) { 
 //            dizi2[i]=Math.sqrt(j);
@@ -37,7 +28,7 @@ public class UygulamaDiziler {
 //            i++;
 //            
 //        }
-        //21
+        
 //      double max=dizi2[0];
 //      double min=dizi2[0];
 //        for (int j =1; j <N; j++) {
@@ -51,8 +42,7 @@ public class UygulamaDiziler {
 //        System.out.println("dizinin en küçük elemanı: "+min);
 //        System.out.println("dizinin en büyük elemanı: "+max);
         
-        //22
-        //19.Soru
+        
           Scanner oku = new Scanner(System.in);
         System.out.println("Dizilerin eleman sayısını giriniz");
         int N = oku.nextInt();
@@ -66,7 +56,7 @@ public class UygulamaDiziler {
         System.out.println(dizi3[0] + " " + dizi3[N - 1]);
         System.out.println("---------------------------");
         
-        //20.Soru
+        
         int i = 0;
         for(double j = 0; j < N; j++)
         {
@@ -74,7 +64,7 @@ public class UygulamaDiziler {
             System.out.println(dizi2[i]);          
         }
         
-        //21.Soru
+        
         double max = dizi2[0];
         double min = dizi2[0];
         for(int j = 1; j < N; j++)
@@ -86,7 +76,7 @@ public class UygulamaDiziler {
         System.out.println("Dizinin En Küçük Elemanı= "+ min);
         System.out.println("Dizinin En Büyük Elemanı= "+ max);
         
-        //22.Soru
+        
         for(int a = 0; a < N; a++)
         {
             int r = (int) (Math.random()*100);
@@ -105,7 +95,7 @@ public class UygulamaDiziler {
         System.out.println(" = " + toplam);
         System.out.println("");
         
-        //23,24,25
+      
         System.out.println("-----------------------------");
         System.out.println(N + " Değerine Tam Bölünenler:");
         int sayac = 0;
