fc711dd6b58398210bf95a0ff5be527510a255e6
Kübra Çetinkaya <35381438+kubracetinkaya@users.noreply.github.com>
2018-03-01 21:25:42 +0200

    Update and rename UygulamaDeğşikenlerveIf.java to Exercises/UygulamaDeğşikenlerveIf.java

--- Exercises/UygulamaDeğşikenlerveIf.java
+++ Exercises/UygulamaDeğşikenlerveIf.java
@@ -1,21 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package uygulama.değşikenlerve.pkgif;
 
 import java.util.Scanner;
 
-/**
- *
- * @author asus
- */
+
 public class UygulamaDeğşikenlerveIf {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
         
        Scanner in=new Scanner(System.in);
